20a789f30d6675be124faedec2bf7baa51944bc0
sanjay <sanjay@sanjay>
2018-06-10 19:59:30 +0530

    Added Service

--- testGit/src/main/java/science/revenswan/testGit/Application.java
+++ testGit/src/main/java/science/revenswan/testGit/Application.java
@@ -1,9 +1,15 @@
 package science.revenswan.testGit;
 
+import science.revenswan.testGit.service.TestService;
+
 public class Application {
 
+	
+	
 	public static void main(String[] args) {
+		TestService service = new TestService();
 		System.out.println("Hello World!");
+		service.service1("sanjay");
 	}
 	
 }
---
+++ testGit/src/main/java/science/revenswan/testGit/service/TestService.java
@@ -0,0 +1,9 @@
+package science.revenswan.testGit.service;
+
+public class TestService {
+
+	public void service1(String name) {
+		System.out.println("Hi " + name);
+	}
+	
+}
